484cee5f2c7f3df38b5b0e4a434e731fb12634aa
lawrencemark <marklawrence@hotmail.co.uk>
2021-09-28 18:13:54 +0100

    adding heroku script

--- dockerheropush.sh
+++ dockerheropush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -f Dockerfile.web . -t $TAG 
-#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-#heroku plugins:install @heroku-cli/plugin-container-registry
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username $DOCKERUSER --password $DOCKERPASSWORD
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
 docker tag $TAG registry.heroku.com/$HEROKU_APP_NAME/web
