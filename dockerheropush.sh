1cd943494b1e796065d124f941465c5772c8e45a
lawrencemark <marklawrence@hotmail.co.uk>
2021-09-29 11:04:03 +0100

    heroku app

--- dockerheropush.sh
+++ dockerheropush.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-docker build -f Dockerfile.web . -t $TAG 
+docker build -f Dockerfile.web . -t todoapp-prod
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 docker login --username $DOCKERUSER --password $DOCKERPASSWORD
 docker login --username _ --password=$HEROKU_API_KEY registry.heroku.com
-docker tag $TAG registry.heroku.com/$HEROKU_APP_NAME/web
+docker tag todoapp-prod registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
 heroku container:release web --app $HEROKU_APP_NAME
